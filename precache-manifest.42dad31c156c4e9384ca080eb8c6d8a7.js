4656cd9d702b71f71624ab7324abda7898f3dd8f
Alexander <alexander@Users-MacBook-Pro.local>
2020-09-01 14:30:47 +0300

    Updates

---
+++ precache-manifest.42dad31c156c4e9384ca080eb8c6d8a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9378bb754259ae56115f0344e151a593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d367a97281af6000f6f4",
+    "url": "/static/css/main.f021dd61.chunk.css"
+  },
+  {
+    "revision": "4ab4eb08912b2a510775",
+    "url": "/static/js/2.2d74a350.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2d74a350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d367a97281af6000f6f4",
+    "url": "/static/js/main.33de135b.chunk.js"
+  },
+  {
+    "revision": "0494fd76fab0cb8ab935",
+    "url": "/static/js/runtime-main.ca390009.js"
+  },
+  {
+    "revision": "dfb4f45036b1c0199ef4bc78f6c96398",
+    "url": "/static/media/logo.dfb4f450.png"
+  }
+]);
